a7e8ca6c2da84b2975af6ccf4fd48631b99d9126
Tomas Patlan <tomas.patlan.1@gmail.com>
2021-12-03 17:09:30 -0400

    day 2 solution

---
+++ day02/pt1.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+//const input = fs.readFileSync('./testInput').toString();
+const input = fs.readFileSync('./input').toString();
+
+let x = 0;
+let y = 0;
+
+let cmdString = '';
+let valString = '';
+
+// readingCmd is true when it's interpreting the movement command
+// false when it's interpreting the movement value
+let readingCmd = true;
+for(let i = 0; i < input.length; i++)
+{
+	if(readingCmd)
+	{
+		if(input[i] != ' ')
+		{
+			cmdString += input[i];
+		}
+		else
+		{
+			readingCmd = false;
+		}
+	}
+	else
+	{
+		if(input[i] != '\n')
+		{
+			valString += input[i];
+		}
+		else
+		{
+			readingCmd = true;
+
+			value = Number(valString);
+			switch(cmdString)
+			{
+				case "forward":
+					x += value;
+					cmdString = '';
+					valString = '';
+					break;
+
+				case "up":
+					y -= value;
+					cmdString = '';
+					valString = '';
+					break;
+
+				case "down":
+					y += value;
+					cmdString = '';
+					valString = '';
+					break;
+			}
+		}
+	}
+}
+
+console.log(`x: ${x}, y: ${y}`);
+console.log(`final product: ${x * y}`);
